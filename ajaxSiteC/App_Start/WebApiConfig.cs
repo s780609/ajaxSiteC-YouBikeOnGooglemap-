aab8f5a45fdd3b67b6da7b43ed02e308a67bc22d
Tinghuan <s780609@gmail.com>
2020-01-05 16:03:31 +0800

    小修改

--- ajaxSiteC/App_Start/WebApiConfig.cs
+++ ajaxSiteC/App_Start/WebApiConfig.cs
@@ -10,10 +10,10 @@ namespace ajaxSiteC
         public static void Register(HttpConfiguration config)
         {
             // Web API 設定和服務
-
+            config.EnableCors();
             // Web API 路由
             config.MapHttpAttributeRoutes();
-
+            
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
--- ajaxSiteC/Controllers/ShippersController.cs
+++ ajaxSiteC/Controllers/ShippersController.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 using ajaxSiteC.Models;
 
 namespace ajaxSiteC.Controllers
 {
+    [EnableCors(origins:"*",headers:"*",methods:"*")]
     public class ShippersController : ApiController
     {
         private NorthwindEntities db = new NorthwindEntities();
